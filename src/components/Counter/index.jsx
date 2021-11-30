fdf64c16f293117ed206599a6b68283a27cc6331
Teilor Souza Barcelos <tsb-@live.com>
2021-11-29 23:18:24 -0300

    component's state

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+export function Counter() {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+      <h2>{counter}</h2>
+      <button type="button" onClick={() => setCounter(counter + 1)}>+1</button>
+    </div>
+  )
+}
